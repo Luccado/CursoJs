9d00bd201c59d9c3f274afb93228d88363fa9040
luccado <luccadioliveira@gmail.com>
2022-10-28 09:29:02 -0400

    exercício

--- Curso/Aula 12/Index.js
+++ Curso/Aula 12/Index.js
@@ -1,1 +1,18 @@
-alert('sou asdkaksd');
+let varA = 'A'; // B
+let varB = 'B'; // C
+let varC = 'C'; // A
+
+/*
+let varD = varA;
+varA = varB;
+varB = varC;
+varC = varD;
+
+
+console.log(varA, varB, varC);
+minha resolução
+*/
+
+[varA, varB, varC] = [varB, varC, varA];
+
+console.log(varA, varB, varC);
