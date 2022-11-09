9afa49da3f6003b97c4f424daca3961321c1bd0b
luccado <luccadioliveira@gmail.com>
2022-11-09 11:48:18 -0400

    a

--- Curso/Aula 14/Index.js
+++ Curso/Aula 14/Index.js
@@ -1,6 +1,18 @@
-let num1 = 1; //number
-let num2 = 2.5; //number
+// IEEE 754-2008
+let num1 = 0.7; //number
+let num2 = 0.1; //number
 
-console.log(num1.toString() + num2);
-console.log(typeof num1)
+// console.log(num1.toString() + num2);
+// num1 = num1.toString();
+// console.log(num1.toString(2)); binário
+//console.log(num1.toFixed(4)); //arrendondar - o 4 é para a casa decimal
+//console.log(number.isInteger(num1));
+
+//let temp = num1 * 'Olá';
+//console.log(Number.isNaN(temp)); - retorna verdadeiro se não for um número NaN(not a number)
+
+num1 += num2; 
+console.log(num1);
+
+
 
