445e3fdf235913ea53dafad7f1b5604f4c84b659
luccado <luccadioliveira@gmail.com>
2022-11-10 08:05:46 -0400

    Contador

---
+++ Projetos/Contador de pass/Index.js
@@ -0,0 +1,8 @@
+//iniciar com 0
+//a cada clique aumentar o contador
+//aumentar a variável "count" quando o botão é aumentado
+//mudar o "count-el" no HTML e refletir no novo valor
+
+function increment() {
+    console.log("the button was clicked")
+}
