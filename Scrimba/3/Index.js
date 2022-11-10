c81c0ac4a682089b1857711575131a8ea0344e7c
luccado <luccadioliveira@gmail.com>
2022-11-10 07:41:48 -0400

    BonusPoints

---
+++ Scrimba/3/Index.js
@@ -0,0 +1,36 @@
+//let count = 5;
+
+//count + 1
+//count = count + 1; // 5 + 1
+
+
+//count = count + 10;
+
+//console.log(count);
+
+// Criar uma variável "bonusPoints". Inicializar com 50, aumentar para 100 e depois diminuir para 25 e finalizar aumentando para 70
+
+let bonusPoints = 50;
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints * 2;
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints / 4;
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints + 45;
+console.log(bonusPoints);
+
+//Solução dele --------------
+//let bonusPoints = 50
+//console.log(bonusPoints)
+
+//bonusPoints = bonusPoints + 50
+//console.log(bonusPoints)
+
+//bonusPoints = bonusPoints - 75
+//console.log(bonusPoints)
+
+//bonusPoints = bonusPoints + 45
+//console.log(bonusPoints)
