642dc903d47f1f38b3fb27cca79410dadec2c1e2
luccado <luccadioliveira@gmail.com>
2022-11-10 07:22:51 -0400

    a

---
+++ Scrimba/2/Index.js
@@ -0,0 +1,17 @@
+// let firstBatch = 5;
+// let secontBatch = 7;
+
+// let count = firstBatch + secontBatch;
+
+//let count = 5 / 7;
+
+// console.log(count);
+
+//create my age in dog age ~ ratio 1 to 7
+
+let myAge = 22;
+let humanDogRatio = 7;
+
+let myDogAge = myAge * humanDogRatio;
+
+console.log(myDogAge);
