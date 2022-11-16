8f3c9a1bcb385d61cb45daa95f39dc7f44b90a12
luccado <luccadioliveira@gmail.com>
2022-11-16 08:14:39 -0400

    Up to date

---
+++ Scrimba/14/index.js
@@ -0,0 +1,10 @@
+
+let errorParagraph = document.getElementById("error");
+console.log(errorParagraph);
+
+function purchase() {
+
+    console.log("clicou");
+    errorParagraph.textContent = "Something went wrong, please try again"
+
+}
