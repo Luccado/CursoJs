569c5f6c2c5f7889240307c7d4d0760285b69878
luccado <luccadioliveira@gmail.com>
2022-11-16 08:26:16 -0400

    a

--- Scrimba/14/index.js
+++ Scrimba/14/index.js
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-let myPoints = 3
-let trespont = 0
- 
-// Create two functions, add3Points() and remove1Point(), and have them
-// add/remove points to/from the myPoints variable
- 
-function add3Points (){
-    myPoints += 3;
-}
-=======
-<<<<<<< HEAD
->>>>>>> 4928f47e06135e13764df86038f5eec0bbaa5a2a
-
- function remove1Point(){
-    myPoints -= 1;
- }
-
-add3Points();
-add3Points();
-add3Points();
-remove1Point();
-remove1Point();
-
-<<<<<<< HEAD
- 
-// Call the functions to that the line below logs out 10
-console.log(myPoints)
-=======
-    console.log("clicou");
-    errorParagraph.textContent = "Something went wrong, please try again"
-
-}
-=======
 let myPoints = 3
 let trespont = 0
  
@@ -55,5 +21,3 @@ remove1Point();
  
 // Call the functions to that the line below logs out 10
 console.log(myPoints)
->>>>>>> 76efad4aeea96b5469bac62e8b7c5666fbcbc7c8
->>>>>>> 4928f47e06135e13764df86038f5eec0bbaa5a2a
