77700929641dc344ae2607d99dbce2cd1700a77d
luccado <luccadioliveira@gmail.com>
2022-11-29 11:17:55 -0400

    calculator challenge

--- Scrimba/17/index.js
+++ Scrimba/17/index.js
@@ -1,4 +1,4 @@
-let errorParagraph = document.getElementById("error");
+/*let errorParagraph = document.getElementById("error");
 console.log(errorParagraph);
 
 function purchase() {
@@ -6,5 +6,12 @@ function purchase() {
     console.log("clicou");
     errorParagraph.textContent = "Something went wrong, please try again"
 
+}*/
+
+let errorParagraph = document.getElementById("error")
+console.log(errorParagraph)
+
+function purchase() {
+    console.log("button clicked")
+    errorParagraph.textContent = "Something went wrong, please try again"
 }
-"to maluco"
