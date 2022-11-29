16a835358eebcdba8ba226943a2184475401e022
luccado <luccadioliveira@gmail.com>
2022-11-29 12:37:36 -0400

    guest inc

--- Scrimba/Placar basquete/index.js
+++ Scrimba/Placar basquete/index.js
@@ -13,3 +13,21 @@ function add2h() {
     console.log(placar1);
     document.getElementById("placar1").textContent = placar1
 }
+
+function add3h() {
+    placar1 = placar1 + 3;
+    console.log(placar1);
+    document.getElementById("placar1").textContent = placar1
+}
+
+//Guest -----
+
+let placarguest = document.getElementById("placarguest");
+console.log(Placar2);
+placarguest = 0;
+
+console.log(plcar2)
+function add1g(){
+    placar2 = placar2 + 1;
+    document.getElementById("placarguest").textContent = placar2
+}
