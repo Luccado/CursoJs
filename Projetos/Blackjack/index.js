3b3276d18e8787b08ab29e3dfb42c9e8acb9dfdc
luccado <luccadioliveira@gmail.com>
2022-12-02 11:16:57 -0400

    Mais coisas

--- Projetos/Blackjack/index.js
+++ Projetos/Blackjack/index.js
@@ -1,12 +1,34 @@
 let firstCard = 10;
 let secondCard = 12;
+let hasBlackJack = false
+let isAlive = true
+let message = "";
 
 let sum = firstCard + secondCard;
 
-if (sum < 21) {
+/*if (sum < 21) {
     console.log("Do you want to draw a new card?")
 } else if (sum === 21) {
     console.log("Ganhou mané");
 } else {
     console.log("Perdeu mané");
+}*/
+
+if (sum <= 20) {
+    message = "Do you want to draw a new card? 🙂"
 }
+
+else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 🥳"
+    hasBlackJack = true
+}
+
+else {
+    message = "You're out of the game! 😭"
+    isAlive = false
+}
+
+// CASH OUT!
+//console.log(hasBlackJack);
+//console.log(isAlive)
+//console.log(message);
---
+++ Scrimba/21/index.js
@@ -0,0 +1,7 @@
+console.log(4 === 3); // False
+console.log(5 > 2); // True
+console.log(12 > 12); // False
+console.log(3 < 0); // False
+console.log(3 >= 3); // True
+console.log(11 <= 11); // True
+console.log(3 <= 2); // False
