bfad5f677a18407971eeaf1f2ffd1600835d4b0e
luccado <luccadioliveira@gmail.com>
2022-12-20 13:06:45 -0400

    Aulas

---
+++ Curso/Aula 17/index.js
@@ -0,0 +1,24 @@
+/*
+Operadores lógicos
+&& -> AND -> E
+|| -> OR -> OU
+! -> NOT -> NÃO
+*/
+
+const expressaoAnd = true && true && false && true;
+console.log(expressaoAnd);
+
+const expressaoOr = true || false
+console.log(expressaoOr)
+
+console.log("-----------")
+
+const usuario = "lucca"; //form usuário
+const senha = "123456"; //form usuário
+
+const vaiLogar = usuario === "lucca" && senha === '123456'
+console.log(vaiLogar);
+
+console.log(!!!true)
+
+
