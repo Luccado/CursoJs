880d0e6e559495fb3b16769b65b9f021c3d22456
luccado <luccadioliveira@gmail.com>
2022-12-24 12:21:23 -0400

    Mais ifelse

---
+++ Curso/Aula 20/Index.js
@@ -0,0 +1,21 @@
+const numero = 9;
+
+if (numero < 10){
+    console.log("é")
+}
+
+if (numero >= 0 && numero <= 5) {
+    console.log("sim");
+} else if (numero >= 6 && numero <= 8) {
+    console.log("O número está entre 6 e 8.")
+} else if (numero >= 9 && numero <= 11) {
+    console.log("O número está entre 9 e 11.")
+} else {
+    console.log("O número não está entre 0 e 11.")
+} 
+
+
+/*else {
+    console.log("Não");
+}
+*/
