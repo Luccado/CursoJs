d708040dad4b34c57cbfb44edca0576d9869094a
luccado <luccadioliveira@gmail.com>
2022-12-27 10:58:57 -0400

    a

---
+++ Curso/Aula 21/index.js
@@ -0,0 +1,25 @@
+const nome = 'Luccado'
+const alunos = ['Luiz', 'Lucca', 'João']
+
+//alunos.push('Luiza')
+//alunos.push('Eduardo')
+//console.log(alunos.slice(0, -1));
+
+//delete alunos[1];
+
+//const removido = alunos.shift(); //remove do começo e coloca em uma variável
+//const removido = alunos.pop(); //remove do final e coloca em uma variável
+
+//alunos.unshift('Luiza') // adiciona no começo
+//alunos.unshift('fábio')
+
+//alunos.push('Otávio'); //adiciona no fim
+//alunos.push('Fábio');
+
+//alunos[alunos.length] = 'luiza' // adiciona no fim
+//alunos[alunos.length] = 'fábio'
+//alunos[alunos.length] = 'luana'
+console.log(alunos); 
+
+//alunos[0] = "Eduardo" // altera
+//alunos[3] = "Luiza"
