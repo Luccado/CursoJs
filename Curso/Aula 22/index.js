04c46fa25f784cc4764dd33c1a69dfd43ff207b0
luccado <luccadioliveira@gmail.com>
2022-12-28 10:34:53 -0400

    function

---
+++ Curso/Aula 22/index.js
@@ -0,0 +1,20 @@
+//function saudacao(nome) {
+//    return `Bdia ${nome}!`;
+//}
+
+//const variavel = saudacao("Luiz");
+//saudacao("maria");
+//saudacao('felipe')
+//console.log(variavel)
+
+//function soma(x = 1, y = 1) {
+//    const resultado = x + y;
+//    return resultado
+//}
+
+const raiz = n => n ** 0.5;
+
+console.log(raiz(9))
+
+//console.log(soma(2, 7));
+//console.log(resultado) - vai dar erro pq a variável tá dentro da função
