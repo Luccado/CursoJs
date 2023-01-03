bdf03e0dd6462680d0c19b8555b93bd1dc1c5452
luccado <luccadioliveira@gmail.com>
2023-01-03 14:05:04 -0400

    a

---
+++ Curso/Aula 29/index.js
@@ -0,0 +1,40 @@
+const verdadeira = true
+
+// // // let tem escopo de bloco {... bloco}
+// // // var só tem escopo de função
+
+// // let nome = 'Luiz'; // criando
+// // var nome2 = 'Lucca'; // criando 
+
+// // var nome2 = 'Souza'; // redeclarada
+
+// // if (verdadeira) {
+// //     let nome = 'Otávio'; //criando
+// //     console.log(nome, nome2); 
+
+// //     if (verdadeira) {
+// //         let nome = 'Outra coisa' 
+// //         console.log(nome, nome2);
+// //     }
+// // }
+// var sobrenome = 'Souza';
+
+// function falaOi() {
+// //    var nome = 'Lucca';
+//     console.log(sobrenome);
+
+//     if(verdadeira) {
+//         console.log(sobrenome);
+//         let nome = 'Lucca';
+//         console.log(nome);
+//     }
+// //    console.log(nome); foi declarado dentro de outro bloco, logo não vai dar certo
+// }
+
+// //console.log(nome); // fora do escopo
+// falaOi();
+
+
+console.log(sobrenome);
+
+let sobrenome = 'Lucca';
