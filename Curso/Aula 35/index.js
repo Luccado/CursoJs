8ceae85346df4acbf3d4c3e41d7109b2a9cc23f9
luccado <luccadioliveira@gmail.com>
2023-02-17 12:06:48 -0400

    comesbebes

--- Curso/Aula 35/index.js
+++ Curso/Aula 35/index.js
@@ -1,3 +1,32 @@
+const pessoa = {
+    nome: 'Lucca',
+    sobrenome: 'Souza'
+};
+
+for (let chave in pessoa){
+    console.log(chave, pessoa[chave]);
+}
+
 //             01
-const nome = 'Lucca';
-const idade = 11;
+//const nome = 'Lucca';
+//const nome = ['Lucca', 'Luiz', 'Otávio']
+
+
+// //for (let i = 0; i < nome.length; i++) {
+// //    console.log(nome[i])
+// //}
+
+
+// // for (let i in nome) {
+// //     console.log(nome[i]);
+// // }
+
+// for (let valor of nome) {
+//     console.log(valor);
+// }
+
+// console.log('#####')
+
+// nome.forEach(function(el){
+//     console.log(el)
+// });
