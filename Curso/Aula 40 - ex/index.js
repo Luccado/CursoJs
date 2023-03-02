5bd4815644f710f47071ea4aed703ce22c501257
luccado <luccadioliveira@gmail.com>
2023-03-02 10:28:07 -0400

    terminar

---
+++ Curso/Aula 40 - ex/index.js
@@ -0,0 +1,12 @@
+// Escreva uma função chamada ePaisagem que recebe dois argumentos, largura e altura de uma Imagem (number). 
+// retorne true se a imagem estiver no modo paisagem
+
+// const ePaisagem = (largura, altura) => largura > altura ? true : false;
+
+// console.log(ePaisagem(100,200))
+
+//resposta
+
+const ePaisagem = (largura, altura) => largura > altura;
+
+console.log(ePaisagem(1920, 1080));
---
+++ Curso/Aula 41 - ex/index.js
@@ -0,0 +1,16 @@
+// Escreva uma função que recebe um número e retorne o seguinte:
+// Número é divisível por 3 = Fizz
+// Número é divisível por 5 = Buzz
+// Número é divisível por 3 e 5 = FizzBuzz
+// Número NÃO é divisível por 3 e 5 = retorna o próprio número
+// Checar se o número é realmente um número
+// Use a função com número de 0 a 100
+
+let x = 3
+function FizzBuzz(x) {
+    if (x / 3 % 1) {
+        console.log('Fizz')
+    }
+}
+
+console.log(FizzBuzz(3))
