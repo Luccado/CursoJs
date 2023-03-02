6753c9707a7590658522ee7bc560139747c9d96f
luccado <luccadioliveira@gmail.com>
2023-03-02 10:28:53 -0400

    já já

--- Curso/Aula 41 - ex/index.js
+++ Curso/Aula 41 - ex/index.js
@@ -4,7 +4,7 @@
 // Número é divisível por 3 e 5 = FizzBuzz
 // Número NÃO é divisível por 3 e 5 = retorna o próprio número
 // Checar se o número é realmente um número
-// Use a função com número de 0 a 100
+// Use a função com número de 0 a 100 - while i <= 100
 
 let x = 3
 function FizzBuzz(x) {
