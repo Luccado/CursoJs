dee4cb408b08c59d296d0092d73f3686d5a31740
luccado <luccadioliveira@gmail.com>
2023-03-02 09:34:24 -0400

    Beatriz

---
+++ Curso/Aula 39 - ex/index.js
@@ -0,0 +1,25 @@
+// Escreva uma função que escreva 2 números e retorne o maior deles
+
+// let num1 = 4;
+// let num2 = 3;
+
+// function maior() {
+//     if (num1 > num2) {
+//         return num1;
+//     } else {
+//         return num2
+//     }
+// }
+
+// console.log(maior());
+
+// RESOLUÇÃO ------------------------------
+
+// function max(x,y) {
+//     return x > y ? x : y; // se x > y RETORNE x ENTÃO y
+// }
+
+const max2 = (x,y) => x > y ? x : y;
+
+
+console.log(max2(100,20));
