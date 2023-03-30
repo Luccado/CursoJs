c1f6af1549904c43dfc695f31389c3c11f7d1087
luccado <luccadioliveira@gmail.com>
2023-03-30 11:42:52 -0400

    slider

--- Projetos/Projeto pessoal/Modelo/assets/js/main.js
+++ Projetos/Projeto pessoal/Modelo/assets/js/main.js
@@ -6,3 +6,20 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+
+
+
+  let count = 1;
+  document.getElementById("radio1").checked = true;
+
+  setInterval( function(){
+    nextImage()
+  },5000)
+
+  function nextImage(){
+    count++;
+    if(count>4){
+      count = 1;
+    }
+    document.getElementById("radio"+count).checked = true;
+  }
