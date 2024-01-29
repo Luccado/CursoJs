20052c671a5a65fb825e4130cecc6824b103328d
luccado <luccadioliveira@gmail.com>
2024-01-29 11:27:18 -0400

    s

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,7 +1,10 @@
 alert("Oiiii");
 let numeroSecreto = 29;
+console.log(numeroSecreto);
 let chute = prompt("Escolha um número entre 1 e 30");
 
 if (numeroSecreto == chute) {
-    console.log("Deu certo!")
+    alert(`Deu certo! o número era ${numeroSecreto}`)
+} else {
+    alert("Tá errado");
 }
